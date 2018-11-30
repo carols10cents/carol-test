ca0be18ad1a6982b26a8cbbc384393c63d334afa
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2018-11-30 16:01:28 -0500

    error example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::error::Error;
+use std::fs::File;
+
+fn num_documents_created_in_last_minute() -> u8 {
+    2
+}
+
+const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
+
+fn create_document(filename: &str) -> Result<File, Box<Error>> {
+    if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
+        return Err("You have exceeded the allowed number of documents per minute".into());
+    }
+
+    Ok(File::create(filename)?)
+}
+
 fn main() {
-    println!("Hello, world!");
 }
